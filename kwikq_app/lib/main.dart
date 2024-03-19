80d7dc1492155cb9715911670a5b51b474563e01
Darker621 <64254377+Darker621@users.noreply.github.com>
2024-03-19 22:39:17 +0530

    5th commit

--- kwikq_app/lib/main.dart
+++ kwikq_app/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:kwikq_app/admin/admin_login.dart';
 import 'package:kwikq_app/firebase_options.dart';
 import 'package:kwikq_app/pages/auth_page.dart';
-import 'package:kwikq_app/pages/onboard.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- kwikq_app/lib/pages/home.dart
+++ kwikq_app/lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -12,52 +13,175 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool icecream = false, pizza = false, salad = false, burger = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.only(top: 20, left: 20, right: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Hello User,",
-                      style: GoogleFonts.poppins(
-                          fontSize: 20, fontWeight: FontWeight.bold)),
-                  Container(
-                    padding: EdgeInsets.all(4),
-                    decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(4)),
-                    child: Icon(
-                      Icons.shopping_cart,
-                      color: Colors.white,
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Delicious Food",
-                style: GoogleFonts.poppins(
-                    fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "Discover and Get Great Food",
-                style: GoogleFonts.poppins(
-                    textStyle: TextStyle(color: Colors.grey, letterSpacing: .5),
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold),
-              ),
-            ],
+        body: SafeArea(
+            child: Container(
+                margin: EdgeInsets.only(top: 20, left: 20, right: 10),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Hello User,",
+                              style: GoogleFonts.poppins(
+                                  fontSize: 20, fontWeight: FontWeight.bold)),
+                          Container(
+                            padding: EdgeInsets.all(4),
+                            decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(4)),
+                            child: Icon(
+                              Icons.shopping_cart,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Delicious Food",
+                        style: GoogleFonts.poppins(
+                            fontSize: 25, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Discover and Get Great Food",
+                        style: GoogleFonts.poppins(
+                            textStyle: TextStyle(
+                                color: Colors.grey, letterSpacing: .5),
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      showitem(),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      Row(children: [
+                        Container(
+                          child: Column(children: [
+                            Image.asset(
+                              "images/salad2.png",
+                              height: 150,
+                              width: 150,
+                              fit: BoxFit.cover,
+                            ),
+                            Text(
+                              "Veggie Taco Hash",
+                              style: GoogleFonts.poppins(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            )
+                          ]),
+                        ),
+                      ])
+                    ]))));
+  }
+
+  Widget showitem() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        GestureDetector(
+          onTap: () {
+            icecream = false;
+            pizza = true;
+            salad = false;
+            burger = false;
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: pizza ? Colors.black : Colors.white,
+                  borderRadius: BorderRadius.circular(10)),
+              padding: EdgeInsets.all(8),
+              child: Image.asset("images/pizza.png",
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                  color: pizza ? Colors.white : Colors.black),
+            ),
           ),
         ),
-      ),
+        GestureDetector(
+          onTap: () {
+            icecream = true;
+            pizza = false;
+            salad = false;
+            burger = false;
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: icecream ? Colors.black : Colors.white,
+                  borderRadius: BorderRadius.circular(10)),
+              padding: EdgeInsets.all(8),
+              child: Image.asset("images/ice-cream.png",
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                  color: icecream ? Colors.white : Colors.black),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            icecream = false;
+            pizza = false;
+            salad = true;
+            burger = false;
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: salad ? Colors.black : Colors.white,
+                  borderRadius: BorderRadius.circular(10)),
+              padding: EdgeInsets.all(8),
+              child: Image.asset("images/salad.png",
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                  color: salad ? Colors.white : Colors.black),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            icecream = false;
+            pizza = false;
+            salad = false;
+            burger = true;
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: burger ? Colors.black : Colors.white,
+                  borderRadius: BorderRadius.circular(10)),
+              padding: EdgeInsets.all(8),
+              child: Image.asset("images/burger.png",
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                  color: burger ? Colors.white : Colors.black),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
